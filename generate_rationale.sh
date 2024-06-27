658112099e5e94b5d2c37d90e65e04090a6da966
weizhepei <zhepei.wei@gmail.com>
2024-06-27 17:54:13 -0400

    set cache_dir optional

--- generate_rationale.sh
+++ generate_rationale.sh
@@ -5,5 +5,4 @@ CUDA_VISIBLE_DEVICES=0 python src/inference.py \
   --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct \
   --n_docs 5 \
   --output_dir dataset/${DATASET}\
-  --cache_dir $CACHE_DIR \
   --do_rationale_generation \
